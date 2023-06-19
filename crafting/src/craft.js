56a7d2a6799216a7c9d20eaa348f3241a489c6fe
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2023-06-19 10:01:23 -0600

    complete crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
+  constructor(params) {
+    this.name = params['type'];
+    this.materials = params['materials'];
+    this.completed = false;
 
+    this.completeCraft = function() {
+      this.completed = true;
+      return 'All done! It looks great!';
+    }
+
+    this.calculateProjectTotal = function() {
+      var totalCost = 0;
+      for (var i = 0; i < this.materials.length; i++) {
+        totalCost += this.materials[i].calculateMaterialCost();
+      }
+      return totalCost;
+    }
+;  }
 }
 
 module.exports = Craft;
