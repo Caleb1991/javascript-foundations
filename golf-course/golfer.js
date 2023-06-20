2bbbe88d495e0ebf2463086a0b47ba8127f29eb2
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2023-06-19 19:01:01 -0600

    Golf course

--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,51 @@
 class Golfer {
+  constructor(params) {
+    this.name = params['name'];
+    this.handicap = params['handicap'];
+    this.frustration = 0;
+    this.confidence = 0;
 
+    this.calculateAvg = function(par) {
+      var average = par + this.handicap;
+      return `I usually shoot a ${average} on average.`;
+    }
 
+    this.playRound = function(course) {
+      var difficultyFrustration = { hard: 500, moderate: 100 };
+      this.frustration += difficultyFrustration[`${course.difficulty}`];
+    }
+
+    this.hitTheRange = function() {
+      this.confidence += 10;
+    }
+
+    this.marvel = function(course) {
+      var statement = "I love the "
+
+      for (var i = 0; i < course.features.length; i++) {
+        statement += course.features[i];
+        if (i != course.features.length - 1) {
+          statement += ' and ';
+        } else {
+          statement += ' on this course!';
+        }
+      }
+      return statement
+    }
+
+    this.whatYaShoot = function(score) {
+      if (score < 0 ) {
+        this.frustration = 0;
+        return 'I AM THE GREATEST GOLFER ALIVE!';
+      } else if (score > 0) {
+        this.frustration += 20;
+        return 'Doh!';
+      } else {
+        this.frustration -= 10;
+        return 'Booyah!';
+      }
+    }
+  }
 }
 
 module.exports = Golfer;
