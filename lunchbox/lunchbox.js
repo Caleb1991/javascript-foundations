66e793603919cbbbcbe9812ae00388b5fd1035ea
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2023-06-20 11:50:09 -0600

    complete lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,27 @@
 class LunchBox {
+  constructor(params) {
+    this.owner = params['owner'];
+    this.material = params['madeOf'];
+    this.shape = params['shape'];
+    this.color = params['color'];
+    this.snacks = [];
 
+    this.acquireSnack = function(snack) {
+      this.snacks.push(snack);
+      snack.isInLunchBox = true;
+    }
+
+    this.findHealthySnacks = function() {
+      let tempArray = [];
+      for (const snack of this.snacks) {
+        if (snack.checkForHealthy()) {
+          tempArray.push(snack.type);
+        }
+      }
+
+      return tempArray;
+    }
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,22 @@
 class Snack {
+  constructor(type) {
+    this.deliciousLevel = 'extra';
+    this.type = type;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
 
+    this.getEaten = function(qty=10) {
+      this.amount -= qty;
+      if (this.amount == 20) {
+        this.cuttingItClose = true;
+      }
+    }
+
+    this.checkForHealthy = function() {
+      return this.type.toLowerCase().includes('fruit');
+    }
+  }
 }
 
 module.exports = Snack;
