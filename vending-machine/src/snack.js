c87a4e41c213d4b7ea48483ff558d404868f1525
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2023-06-22 15:55:17 -0600

    vending machine

--- vending-machine/src/snack.js
+++ vending-machine/src/snack.js
@@ -1,5 +1,22 @@
 class Snack {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.itemsInStock = 0;
 
+    this.stockItems = function(amount) {
+      this.itemsInStock += amount;
+    }
+
+    this.removeItem = function() {
+      if (this.itemsInStock) {
+        this.itemsInStock--;
+        return `Item taken! There are now ${this.itemsInStock} items left.`;
+      } else {
+        return `Sorry, no ${this.name} left in stock!`;
+      }
+    }
+  }
 }
 
 module.exports = Snack;
--- vending-machine/src/vending-machine.js
+++ vending-machine/src/vending-machine.js
@@ -1,5 +1,41 @@
 class VendingMachine {
+  constructor(params) {
+    this.id = params.id;
+    this.isBroken = params.isBroken;
+    this.snacks = [];
 
+    this.addSnacks = function(snack) {
+      if (!this.alreadyStocked(snack.name)) {
+        this.snacks.push(snack);
+      } else {
+        return 'Sorry, that snack is already stocked! Try adding a different snack.';
+      }
+    }
+
+    this.alreadyStocked = function(snackName) {
+      for (var snack of this.snacks) {
+        if (snack.name == snackName) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    this.purchaseSnack = function(name, amount) {
+      for (var snack of this.snacks) {
+        if (snack.name == name) {
+          if (snack.itemsInStock == 0) {
+            return 'Sorry, no items in stock. Try another item.';
+          } else if (snack.price > amount) {
+            return 'Sorry, not enough payment. Please add more money.';
+          }
+          var change = amount - snack.price; 
+          snack.removeItem();
+          return `Success! Here is $${change} back!`;
+        }
+      }
+    }
+  }
 }
 
 module.exports = VendingMachine;
